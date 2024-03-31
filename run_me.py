959c6d1d97d8f7e41e5b045fdc2f3f7091aba9c5
uzakotim <timauzakov@gmail.com>
2024-03-31 18:11:11 +0500

    uploaded first version

---
+++ run_me.py
@@ -0,0 +1,26 @@
+import subprocess
+import json
+
+# BEFORE running this code
+# configure parameters in config.json
+# and add a line config.json in .gitignore
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# command = f'nmcli dev wifi connect {config["ssid"]} password {config["password"]}'
+command = f'echo {config["ssid"]}'
+commands = [
+    f'sudo nmcli radio wifi on',
+    f'sudo nmcli dev wifi connect {config["ssid"]} password {config["password"]}',
+    f'ping google.com',
+    f'sudo nmcli con up {config["ssid"]}',
+    f'sudo nmcli con mod {config["ssid"]} ipv4.addresses {config["ip"]}/24',
+    f'sudo nmcli con mod {config["ssid"]} ipv4.gateway {config["gateway"]}',
+    f'sudo nmcli con mod {config["ssid"]} ipv4.dns {config["dns"]}',
+    f'sudo nmcli con mod {config["ssid"]} ipv4.method manual',
+    f'sudo nmcli con up {config["ssid"]}',
+    f'ip a'
+]
+for command in commands:
+    subprocess.run(command, shell=True)
